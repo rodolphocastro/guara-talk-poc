20f33820c0c18a25f31e9e12c25814cfefee4a71
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-07-03 15:13:34 -0300

    test: Properly mock Firebase call on PrincipiosList

--- src/PrincipiosList.test.tsx
+++ src/PrincipiosList.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { Principio } from "./Principio";
+import * as principioDeps from "./Principio"
 import PrincipiosList from "./PrincipiosList";
 
 test('should render a list of empty Principios', () => {
-    jest.mock("./Principio", () => ({
-        fetchFromFirebase: jest.fn().mockResolvedValue([])
-    }))
+    jest
+        .spyOn(principioDeps, "fetchFromFirebase")
+        .mockResolvedValue([])
     const element = render(<PrincipiosList />)
     const principiosHeader = element.getByLabelText('QuantidadePrincipios');
     expect(principiosHeader).toBeInTheDocument();
@@ -27,9 +28,11 @@ test('should render a list of many Principios', async () => {
         }
     ]
 
-    jest.mock("./Principio", () => ({
-        fetchFromFirebase: jest.fn().mockResolvedValue(somePrincipios)
-    }))
+    // SpyOn faz com que o Jest "observe" a chamada a alguma coisa
+    // seguido do mock<algumacoisa> faz com que ele sequestre a execução e retorne o que vc deseja.
+    jest
+        .spyOn(principioDeps, "fetchFromFirebase")
+        .mockResolvedValue(somePrincipios)
 
     act(() => { render(<PrincipiosList />) })
     const principiosHeader = screen.getByLabelText('QuantidadePrincipios');
